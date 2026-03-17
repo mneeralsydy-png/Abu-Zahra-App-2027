77b684fdbecfb262463268034b3bd04e145c069d
mneeralsydy-png <mneeralsydy@gmail.com>
2026-03-17 05:21:25 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.privatedialer.app',
-  appName: 'Private Dialer',
+  appId: 'com.munir.abuzahra',
+  appName: 'Abu Zahra',
   webDir: 'public',
   server: {
     androidScheme: 'https'
-  },
-  plugins: {
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      serverClientId: 'YOUR_SERVER_CLIENT_ID.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
-    },
-  },
+  }
 };
 
 export default config;
